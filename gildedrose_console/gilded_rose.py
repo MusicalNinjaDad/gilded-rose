dc9db83ff198d68747e62c01c38f660f84c9526b
Mike Foster <i.am.mike.foster@gmail.com>
2024-02-20 21:56:40 +0100

    ensure quality never goes above 50

--- gildedrose_console/gilded_rose.py
+++ gildedrose_console/gilded_rose.py
@@ -11,15 +11,12 @@ class GildedRose(object):
                 if item.name != "Sulfuras, Hand of Ragnaros":
                     item.quality = item.quality - 1
             else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+                item.quality = item.quality + 1
+                if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if item.sell_in < 11:
+                        item.quality = item.quality + 1
+                    if item.sell_in < 6:
+                        item.quality = item.quality + 1
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
@@ -30,10 +27,11 @@ class GildedRose(object):
                     else:
                         item.quality = item.quality - item.quality
                 else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                    item.quality = item.quality + 1
             if item.quality < 0:
                 item.quality = 0
+            if item.quality > 50 and item.name != "Sulfuras, Hand of Ragnaros":
+                item.quality = 50
 
     def __eq__(self, _other) -> bool:
         try:
--- test/test_gildedrose.py
+++ test/test_gildedrose.py
@@ -22,3 +22,34 @@ def test_qualitynevernegative(items):
     gr = GildedRose(items=items)
     gr.update_quality()
     assert gr.items[0].quality >= 0
+
+@pytest.mark.parametrize(
+    "items",
+    [
+        pytest.param(
+            Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+            marks=pytest.mark.xfail(reason="Sulfuras quality always 80"), 
+            id="Sulfuras"
+        ),
+        pytest.param(
+            [Item(name="Aged Brie", sell_in=2, quality=50)],
+            id="Aged Brie"
+        ),
+        pytest.param(
+            [Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=2, quality=50)],
+            id="Backstage passes +3"
+        ),
+        pytest.param(
+            [Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=8, quality=50)],
+            id="Backstage passes +2"
+        ),
+        pytest.param(
+            [Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=18, quality=50)],
+            id="Backstage passes +1"
+        ),
+    ]
+)
+def test_qualitymax50(items):
+    gr = GildedRose(items=items)
+    gr.update_quality()
+    assert gr.items[0].quality <= 50
