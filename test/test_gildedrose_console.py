7ab05b591d043e69ca79f3a357ced2faa4e9189e
Mike Foster <i.am.mike.foster@gmail.com>
2024-02-20 18:25:18 +0100

    RED: test for expected values after update_quality

--- test/test_gildedrose_console.py
+++ test/test_gildedrose_console.py
@@ -15,14 +15,14 @@ def test_everything():
             ]
     )
     expected = GildedRose(items = [
-             Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
-             Item(name="Aged Brie", sell_in=2, quality=0),
-             Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
+             Item(name="+5 Dexterity Vest", sell_in=9, quality=19),
+             Item(name="Aged Brie", sell_in=1, quality=1),
+             Item(name="Elixir of the Mongoose", sell_in=4, quality=6),
              Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
              Item(
                  name="Backstage passes to a TAFKAL80ETC concert", 
-                 sell_in=15, 
-                 quality=20
+                 sell_in=14, 
+                 quality=21
                  )
             ]
     )
@@ -32,15 +32,3 @@ def test_everything():
         assert appitem.quality == expecteditem.quality
  
     # app.update_quality()
-    # assert app == GildedRose(items = [
-    #          Item(name="+5 Dexterity Vest", sell_in=9, quality=19),
-    #          Item(name="Aged Brie", sell_in=1, quality=1),
-    #          Item(name="Elixir of the Mongoose", sell_in=4, quality=6),
-    #          Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
-    #          Item(
-    #              name="Backstage passes to a TAFKAL80ETC concert", 
-    #              sell_in=14, 
-    #              quality=21
-    #              )
-    #         ]
-    # )
