8f71c8dffa260fe0bc9d3050df3ae4740964a584
Mike Foster <i.am.mike.foster@gmail.com>
2024-02-20 21:28:25 +0100

    quality never negative

--- gildedrose_console/gilded_rose.py
+++ gildedrose_console/gilded_rose.py
@@ -8,9 +8,8 @@ class GildedRose(object):
     def update_quality(self):
         for item in self.items:
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+                if item.name != "Sulfuras, Hand of Ragnaros":
+                    item.quality = item.quality - 1
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
@@ -26,14 +25,15 @@ class GildedRose(object):
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
+                        if item.name != "Sulfuras, Hand of Ragnaros":
+                            item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
                 else:
                     if item.quality < 50:
                         item.quality = item.quality + 1
+            if item.quality < 0:
+                item.quality = 0
 
     def __eq__(self, _other) -> bool:
         try:
