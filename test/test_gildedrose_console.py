6179e88268b53ece8e7a79260e6e315b609b0947
Mike Foster <i.am.mike.foster@gmail.com>
2024-02-20 18:18:56 +0100

    test that two gildedroses are equal

---
+++ test/test_gildedrose_console.py
@@ -0,0 +1,46 @@
+from gildedrose_console.gilded_rose import GildedRose, Item
+
+def test_everything():
+# Well almost ... we're not checking running from console here ...
+    app = GildedRose(items = [
+             Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
+             Item(name="Aged Brie", sell_in=2, quality=0),
+             Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
+             Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+             Item(
+                 name="Backstage passes to a TAFKAL80ETC concert", 
+                 sell_in=15, 
+                 quality=20
+                 )
+            ]
+    )
+    expected = GildedRose(items = [
+             Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
+             Item(name="Aged Brie", sell_in=2, quality=0),
+             Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
+             Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+             Item(
+                 name="Backstage passes to a TAFKAL80ETC concert", 
+                 sell_in=15, 
+                 quality=20
+                 )
+            ]
+    )
+    for appitem, expecteditem in zip(app.items, expected.items):
+        assert appitem.name == expecteditem.name
+        assert appitem.sell_in == expecteditem.sell_in
+        assert appitem.quality == expecteditem.quality
+ 
+    # app.update_quality()
+    # assert app == GildedRose(items = [
+    #          Item(name="+5 Dexterity Vest", sell_in=9, quality=19),
+    #          Item(name="Aged Brie", sell_in=1, quality=1),
+    #          Item(name="Elixir of the Mongoose", sell_in=4, quality=6),
+    #          Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+    #          Item(
+    #              name="Backstage passes to a TAFKAL80ETC concert", 
+    #              sell_in=14, 
+    #              quality=21
+    #              )
+    #         ]
+    # )
